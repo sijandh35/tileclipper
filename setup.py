48102fd840b3a8098d369448486b7ca525196cc5
sijandh35 <sumoseason35@gmail.com>
2024-01-16 16:51:04 +0545

    feat: requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as rh:
+    install_requires = rh.read()
+
 setup(
     name='tileclipper',
     version='0.8',
@@ -12,11 +15,15 @@ setup(
     packages=find_packages(),
     package_data={'tileclipper': ['tileclipper/docs/user_guide.md']},
     license='GPLv3',
-    install_requires=[
-        'requests',
-        'pyproj',
-        'tqdm'
-        # Add other dependencies here if needed
+    url='https://github.com/sijandh35/tileclipper',
+    install_requires=install_requires,
+    keywords=['map', 'tile', 'clip', 'download', 'tileclipper'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3.8',
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
